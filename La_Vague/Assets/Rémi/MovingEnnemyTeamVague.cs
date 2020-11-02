cdf719ec59c31322506c213b08ce63f0e1f1e0b4
Karpy <remi.vedrenne.pro@gmail.com>
2020-11-02 18:54:52 +0100

    EnnemyController

---
+++ La_Vague/Assets/Rémi/MovingEnnemyTeamVague.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingEnnemyTeamVague : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public float speed;
+    public EnnemyManagerTeamVague manage;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Moving()
+    {
+        rb.velocity = new Vector2(speed, 0);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Trigger"))
+        {
+            manage.state = EnnemyManagerTeamVague.phase.Replace;
+        }
+    }
+
+    
+}
