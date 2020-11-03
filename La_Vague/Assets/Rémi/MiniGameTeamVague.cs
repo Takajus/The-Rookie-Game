95f6d5d521573602c200e8451594d71f4362448d
Karpy <remi.vedrenne.pro@gmail.com>
2020-11-03 14:04:13 +0100

    Tmp + Game

---
+++ La_Vague/Assets/Rémi/MiniGameTeamVague.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MiniGameTeamVague : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshProUGUI touche1;
+    public TextMeshProUGUI touche2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
