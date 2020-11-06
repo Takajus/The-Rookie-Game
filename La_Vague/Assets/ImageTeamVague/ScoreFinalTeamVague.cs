04b79d5930d0002b884e9c1c7ff1403148965d6c
Karpy <remi.vedrenne.pro@gmail.com>
2020-11-06 11:34:58 +0100

    Ajout score final

---
+++ La_Vague/Assets/ImageTeamVague/ScoreFinalTeamVague.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreFinalTeamVague : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Audioo;
+    public TextMeshProUGUI text;
+    void Start()
+    {
+        Audioo = GameObject.Find("SoundManagerTeamVague");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Votre Score: " + Audioo.GetComponent<AudioManagerTeamVague>().Score.ToString();
+    }
+}
