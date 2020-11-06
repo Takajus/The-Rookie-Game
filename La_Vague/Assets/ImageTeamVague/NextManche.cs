aaa1b30ed33cacce586040cde5979395532c0ac8
Karpy <remi.vedrenne.pro@gmail.com>
2020-11-06 15:46:19 +0100

    no message

---
+++ La_Vague/Assets/ImageTeamVague/NextManche.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextManche : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string naame;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(naame);
+        }
+    }
+}
