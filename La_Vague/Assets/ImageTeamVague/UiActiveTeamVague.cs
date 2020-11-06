e1246573e183eaa8e0c64b0d0850265ac1d664f7
Karpy <remi.vedrenne.pro@gmail.com>
2020-11-06 11:58:25 +0100

    Final Push (enfin)

---
+++ La_Vague/Assets/ImageTeamVague/UiActiveTeamVague.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiActiveTeamVague : MonoBehaviour
+{
+    public KeyCode Touche;
+
+    public bool Active;
+
+    public GameObject Object;
+    void Update()
+    {
+        if (Input.GetKeyDown(Touche))
+        {
+            Active = ActiveUi(Active);
+        }
+    }
+
+    bool ActiveUi(bool active)
+    {
+        Object.SetActive(active);
+        return !active;
+    }
+}
